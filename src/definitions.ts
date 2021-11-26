3f00bc51df1b79907483774d73c44b31c20289f8
Story5 <337995545@qq.com>
2021-11-26 20:04:16 +0800

    步骤一:定义方法签名

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,11 @@
 export interface HciAsrPlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
+  sdkInit(options: InitOptions): Promise<{ value: string }>;
+}
+
+export interface InitOptions {
+  appKey: string;
+  secret: string;
+  sysUrl: string;
+  capUrl: string;
 }
