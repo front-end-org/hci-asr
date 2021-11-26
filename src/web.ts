98dd695a14048bf447c846faf7b9075bd2ff082a
Story5 <337995545@qq.com>
2021-11-26 20:04:40 +0800

    步骤二:web 方法实现

--- src/web.ts
+++ src/web.ts
@@ -1,10 +1,16 @@
 import { WebPlugin } from '@capacitor/core';
 
-import type { HciAsrPlugin } from './definitions';
+import type {
+  HciAsrPlugin,
+  InitOptions,
+} from './definitions';
 
 export class HciAsrWeb extends WebPlugin implements HciAsrPlugin {
   async echo(options: { value: string }): Promise<{ value: string }> {
     console.log('ECHO', options);
     return options;
+  }
+  async sdkInit(options: InitOptions): Promise<{ value: string }> {
+    console.log('sdkInit', options);
   }
 }
